b62ef5bbafbdf1c8ac9690d54ea307956f04105b
Vladimir Akopyan <vladimir.akopyan@lebara.com>
2024-04-24 12:54:19 +0100

    Mutple Dependency calls

--- NewRelic_AppInsights_Conflict/Controllers/WeatherForecastController.cs
+++ NewRelic_AppInsights_Conflict/Controllers/WeatherForecastController.cs
@@ -6,6 +6,8 @@ namespace NewRelic_AppInsights_Conflict.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+        const string baseURL = "https://jsonplaceholder.typicode.com/";
+
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -22,7 +24,15 @@ namespace NewRelic_AppInsights_Conflict.Controllers
         public async Task<IEnumerable<WeatherForecast>> Get()
         {
             var client = new HttpClient();
+            await client.GetAsync($"{baseURL}");
+            await client.PostAsJsonAsync($"{baseURL}posts", "{ \"phone\": \"+1 (975) 458-3685\"}");
+            await client.PutAsJsonAsync($"{baseURL}posts/1", "{ \"phone\": \"+1 (975) 458-3685\"}");
+            await client.PatchAsJsonAsync($"{baseURL}posts/1", "{ \"phone\": \"+1 (975) 458-3685\"}");
+            await client.DeleteAsync($"{baseURL}posts/1");
+            await client.GetAsync($"{baseURL}posts");
             await client.GetAsync("https://google.com");
+            Console.WriteLine("All HTTP requests finished");
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
