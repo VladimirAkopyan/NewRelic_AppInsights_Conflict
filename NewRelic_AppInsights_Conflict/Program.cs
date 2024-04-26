454fb2b04cb9e2f7cf014efb55dcd6f043c39e50
Vladimir Akopyan <Vladimir.Akopyan@lebara.com>
2024-04-26 16:02:38 +0100

    OpenTelmetry Branch

--- NewRelic_AppInsights_Conflict/Program.cs
+++ NewRelic_AppInsights_Conflict/Program.cs
@@ -1,3 +1,9 @@
+using Google.Protobuf.WellKnownTypes;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,6 +16,43 @@ builder.Services.AddApplicationInsightsTelemetry(new Microsoft.ApplicationInsigh
 {
     // SET THIS IN ENV VARIABLES
     ConnectionString = builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]
+});
+
+//OpenTracing: https://learn.microsoft.com/en-us/dotnet/core/diagnostics/observability-with-otel
+var tracingOtlpEndpoint = new Uri("https://otlp.eu01.nr-data.net:4318");
+var NewRelicKey = builder.Configuration["NEW_RELIC_LICENSE_KEY"];
+var otel = builder.Services.AddOpenTelemetry();
+
+// Configure OpenTelemetry Resources with the application name
+otel.ConfigureResource(resource => resource
+    .AddService(serviceName: "RELIC_APPINSIGHT_CONFLICT"));
+
+// Add Metrics for ASP.NET Core and our custom metrics and export to Prometheus
+otel.WithMetrics(metrics => metrics
+    // Metrics provider from OpenTelemetry
+    .AddAspNetCoreInstrumentation()
+    // Metrics provides by ASP.NET Core in .NET 8
+    .AddMeter("Microsoft.AspNetCore.Hosting")
+    .AddMeter("Microsoft.AspNetCore.Server.Kestrel"));
+
+// Add Tracing for ASP.NET Core and our custom ActivitySource and export to Jaeger
+otel.WithTracing(tracing =>
+{
+    tracing.AddAspNetCoreInstrumentation();
+    tracing.AddHttpClientInstrumentation();
+    if (tracingOtlpEndpoint != null)
+    {
+        tracing.AddOtlpExporter(otlpOptions =>
+        {
+            var header = $"api-key={NewRelicKey}";
+            otlpOptions.Endpoint = tracingOtlpEndpoint;
+            otlpOptions.Headers = header;
+        });
+    }
+    else
+    {
+        tracing.AddConsoleExporter();
+    }
 });
 
 var app = builder.Build();
